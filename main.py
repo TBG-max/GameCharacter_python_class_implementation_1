6b0f76cec65461a4f0f10c81c48368e055bbb6ba
TBG-max <cristian.perez@stu.vertexacademies.org>
2026-02-03 19:19:51 +0000

    added some code

--- main.py
+++ main.py
@@ -7,55 +7,65 @@ from module import GameCharacter
 # -----------------------------------------
 # TODO 1:
 # Print a message that welcomes the user to this game.
-
-
+print("Wellcome to this game player")
 # TODO 2:
 # Ask the user to name their character and store it in a variable.
-
+print("What is your character's name?")
+name = input("name:")
 
 # TODO 3:
 # Assign a power and a toughness value to the character.
 # (Use any integers you like between 1 and 20.)
-
+power=13
+toughness=19
 
 # TODO 4:
 # Create a new GameCharacter object for the player.
+player = GameCharacter(name,power, toughness)
 
 
 # TODO 5:
 # Create a new GameCharacter object for an enemy.
 # The enemy's name can be something like "Goblin" or "Dragon".
 # Give the enemy its own power and toughness values.
-
+enemy = GameCharacter("Dragon", 15, 16)
 
 # TODO 6:
 # Print both characters' statuses.
+print("Round 1 Fight")
 
+print(player.get_status())
+print(enemy.get_status())
 
 # TODO 7:
 # Print the player's attack message.
 # Then make the enemy take damage equal to the player's power.
-
+print("It's", name,"'s turn to attack")
+player.attack(enemy.take_damage(player.power))
 
 # TODO 8:
 # Print the enemy's updated status.
-
+print(enemy.get_status())
 
 # TODO 9:
 # Print the enemy's attack message.
 # Then make the player take damage equal to the enemy's power.
-
-
+print("It's", enemy.name, "'s turn to attack")
+enemy.attack(player.take_damage(enemy.power))
+# TODO 9:
+# Print the player's updated status.
+print(player.get_status())                      
 # TODO 10:
 # Update the player's name property to be "Lord " + the original name
+player.name = "Lord " + player.name
 
 # TODO 11: 
 # Heal the player by 100 hitpoints
-
+player.heal(100)
 
 # TODO 11:
 # Print the player's updated status.
-
+print(player.get_status())
 
 # TODO 12:
 # Modify the GameCharacter class so a "defeated" message is printed if health goes to 0 or less
--- module.py
+++ module.py
@@ -6,12 +6,13 @@ class GameCharacter:
         self.power = power
         self.toughness = toughness
         self.health = 100
+    
 
     def attack(self):
         return f"{self.name} attacks with power {self.power}!"
 
     def take_damage(self, amount):
-        self.health -= (amount // self.toughness)
+        self.health -= amount
         return f"{self.name} takes {amount} damage."
 
     def heal(self, amount):
